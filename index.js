690f8d315fd0455024a474ca90982d203e9a9dc3
AlexO05 <alex.ormonov05@gmail.com>
2026-03-19 12:16:17 +0100

    tt

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
- 
+ const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+// 👇 HÄR
+const token = process.env.TOKEN;
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+// 👇 OCH HÄR
+client.login(token);
