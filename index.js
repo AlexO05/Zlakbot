435208071d90721f49dfbaf9ba2effa1e5ec458d
AlexO05 <alex.ormonov05@gmail.com>
2026-03-19 15:03:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
+
 const token = process.env.TOKEN; 
 const PREFIX = "!"; // all commands start with !
 client.once('ready', () => {
@@ -24,32 +25,15 @@ client.on('messageCreate', (message) => {
   const args = message.content.slice(PREFIX.length).trim().split(/ +/);
   const commandName = args.shift().toLowerCase();
 
-
-    const Levels = require("discord-xp");
-    Levels.setURL(process.env.MONGO_URI); // or your MongoDB URL
   // Find the command by name or alias
   const command = Object.values(commands).find(cmd => cmd.aliases.includes(commandName));
   
   if (command) {
     command.execute(message);
   }
-
-  
 });
 
-client.on('messageCreate', async (message) => {
-  if (message.author.bot) return;
 
-  // Random XP per message
-  const randomXP = Math.floor(Math.random() * 10) + 5; // 5-15 XP
-  const user = await Levels.appendXp(message.author.id, message.guild.id, randomXP);
-
-  // Check if user leveled up
-  const userLevel = await Levels.fetch(message.author.id, message.guild.id);
-
-
-
-});
 
 
 // 👇 OCH HÄRsa
