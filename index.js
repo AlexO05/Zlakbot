cbec78b64742c0bf7ba9dd2b27d16896671fdfb2
AlexO05 <alex.ormonov05@gmail.com>
2026-03-19 12:21:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ const client = new Client({
 const token = process.env.TOKEN;
 
 client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
+    console.log(`Logged in as ${client.user.tag}`);
+    const channel = client.channels.cache.get(1222254603840786484);
+  if (channel) {
+    channel.send('Hello world!');
+  } else {
+    console.log('Channel not found!');
+  }
+
+
 });
 
 // 👇 OCH HÄR
