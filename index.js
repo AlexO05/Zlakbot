04abc139a0631ec4afc164a90756f07f527783f6
AlexO05 <alex.ormonov05@gmail.com>
2026-03-19 12:23:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const token = process.env.TOKEN;
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
-    const channel = client.channels.cache.get(1222254603840786484);
+    const channel = client.channels.cache.get('1222254603840786484');
   if (channel) {
     channel.send('Hello world!');
   } else {
